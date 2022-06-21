80026cb0852d90ecfc85631914e6e753cfd1f6ee
oleg <example@gmail.com>
2022-06-21 11:56:53 +0300

    first commit

---
+++ works/JS/script.js
@@ -0,0 +1,51 @@
+"use strict"; // деректива (мы работаемв современном режиме)
+/*
+let number = 4.6;
+
+console.log(4/0); //infinity
+console.log('string' * 9); //Not a number
+
+const person = "Oleg 5"; // ` ' " любые
+
+const bool = true; //or false
+
+console.log(something); //null
+//Объект
+let und;
+console.log(und); //undefined
+
+const obj = {
+    name: "John",
+    age: 25, //описание объекта
+    isMarried: false
+};
+
+console.log(obj.name); //or console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []]; //array частный случай объекта
+console.log(arr[1]);
+*/
+
+//alert('Hello'); 
+
+//const result = confirm("Are you here?"); //da ili net
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18"); //otvet usera, +prompt меняет тип на числовой
+//console.log(typeof(answer)); //tip uznat'
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); // вывести на страницу
+console.log(typeof(answers)); */
+
+// 5 Urok
+const category = 'toys';
+console.log(`hhtps;//someurl.com/${category}`);
+
+const user = 'Oleg';
+alert(`Привет,${user}`);
