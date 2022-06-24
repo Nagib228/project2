d3359358c6388a208ea10a524fdb0383df9947e0
oleg <example@gmail.com>
2022-06-24 06:34:33 +0300

    first commit

--- works/project1/JS/script.js
+++ works/project1/JS/script.js
@@ -1,51 +1,20 @@
-"use strict"; // деректива (мы работаемв современном режиме)
-/*
-let number = 4.6;
+const numberOfFiles = +prompt("Сколько фильмов Вы посмотрели?", "");
 
-console.log(4/0); //infinity
-console.log('string' * 9); //Not a number
 
-const person = "Oleg 5"; // ` ' " любые
-
-const bool = true; //or false
-
-console.log(something); //null
-//Объект
-let und;
-console.log(und); //undefined
-
-const obj = {
-    name: "John",
-    age: 25, //описание объекта
-    isMarried: false
+const personalMovieDB = {
+    count: numberOfFiles,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name); //or console.log(obj["name"]);
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = +prompt("На сколько оцените его?", "");
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []]; //array частный случай объекта
-console.log(arr[1]);
-*/
+personalMovieDB.movies[a] = b;   
+personalMovieDB.movies[c] = d; 
 
-//alert('Hello'); 
-
-//const result = confirm("Are you here?"); //da ili net
-//console.log(result);
-
-//const answer = prompt("Вам есть 18?", "18"); //otvet usera, +prompt меняет тип на числовой
-//console.log(typeof(answer)); //tip uznat'
-
-/* const answers = [];
-
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-document.write(answers); // вывести на страницу
-console.log(typeof(answers)); */
-
-// 5 Urok
-const category = 'toys';
-console.log(`hhtps;//someurl.com/${category}`);
-
-const user = 'Oleg';
-alert(`Привет,${user}`);
+console.log(personalMovieDB);
