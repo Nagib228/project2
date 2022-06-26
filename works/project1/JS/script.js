b9ae8de142081c0055b485505910c9288811eb6f
oleg <example@gmail.com>
2022-06-26 06:18:19 +0300

    change script from lesson 12

--- works/project1/JS/script.js
+++ works/project1/JS/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFiles = +prompt("Сколько фильмов Вы посмотрели?", "");
 
 
@@ -8,13 +10,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+let i = 0;
+do {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его?", "");
+    i++;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b; 
+        console.log("Done");
+    } else {
+        console.log('error');
+        i--;
+    }
+} while (i < 2);
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;   
-personalMovieDB.movies[c] = d; 
+if (personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классичесий зритель");
+} else if (personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
