02b879e1297c8cb468f85eec1c9effe1828165f4
oleg <example@gmail.com>
2022-07-18 11:14:03 +0300

    lesson 13

--- works/project1/JS/script.js
+++ works/project1/JS/script.js
@@ -1,37 +1,34 @@
 "use strict";
 
-const numberOfFiles = +prompt("Сколько фильмов Вы посмотрели?", "");
+let num = 20; //глобальная переменная
 
-
-const personalMovieDB = {
-    count: numberOfFiles,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-let i = 0;
-do {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", "");
-    i++;
-    if (a != null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b; 
-        console.log("Done");
-    } else {
-        console.log('error');
-        i--;
-    }
-} while (i < 2);
-
-if (personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("Вы классичесий зритель");
-} else if (personalMovieDB.count >= 30){
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function showFirstMessage(text){
+    console.log(text);   // function decloration
+    let num = 10; //локальная переменная
 }
 
-console.log(personalMovieDB);
+showFirstMessage("Hello world!");
+console.log(num);
+
+// function calc(a, b){
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+
+function ret() {
+    let num = 50;
+//
+//
+    return num;
+}
+const anothernum = ret();
+console.log(anothernum);
+
+let logger = function() {
+    console.log('Hello'); //function expression
+};
+
+logger();
+
+const calc = (a, b) => a + b; //стрелочная фукнция
